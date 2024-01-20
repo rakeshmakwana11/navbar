01fcd4543255873d7a1afb898869c71faab2a8dd
rakeshmakwana11 <144156339+rakeshmakwana11@users.noreply.github.com>
2024-01-20 10:23:44 +0530

    Add files via upload

---
+++ code.js
@@ -0,0 +1,28 @@
+
+
+
+
+document.addEventListener("DOMContentLoaded",function(){
+    document.querySelector(".manu-close-open").addEventListener("click",function(){
+
+        document.querySelector(".mobile-menu").style.width="100%";
+    });
+
+    document.querySelector(".menu-close-btn").addEventListener("click",function(){
+
+        document.querySelector(".mobile-menu").style.width="0";
+    });
+
+    
+    document.querySelector(".drower-close").addEventListener("click",function(){
+
+        document.querySelector(".search-drower").style.width="0";
+    });
+
+
+
+
+
+
+
+});
